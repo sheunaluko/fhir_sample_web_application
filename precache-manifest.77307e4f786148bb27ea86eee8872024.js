070aef666a3c3b4836a88179aee34e221ef9eed8
Sheun Aluko <oluwa@Sheuns-MacBook-Air.local>
2019-08-06 11:43:59 -0700

    Updates

---
+++ precache-manifest.77307e4f786148bb27ea86eee8872024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cce29e71633bae7a39a79142272d444",
+    "url": "/fhir_sample_web_application/index.html"
+  },
+  {
+    "revision": "478f9fdd374a0032413a",
+    "url": "/fhir_sample_web_application/static/css/main.fbe6a18d.chunk.css"
+  },
+  {
+    "revision": "cee2fcd348b686d07c66",
+    "url": "/fhir_sample_web_application/static/js/2.7ce01dc2.chunk.js"
+  },
+  {
+    "revision": "478f9fdd374a0032413a",
+    "url": "/fhir_sample_web_application/static/js/main.c1484b76.chunk.js"
+  },
+  {
+    "revision": "ba7ee101f4f900828be4",
+    "url": "/fhir_sample_web_application/static/js/runtime~main.1ba393d2.js"
+  }
+]);
